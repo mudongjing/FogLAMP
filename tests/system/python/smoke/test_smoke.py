652757b753324d3deeec444668d32687009c46ee
ashish-jabble <ashish@dianomic.com>
2019-02-13 18:14:39 +0530

    configparser added for smoke tests

--- tests/system/python/smoke/test_smoke.py
+++ tests/system/python/smoke/test_smoke.py
@@ -13,7 +13,7 @@ import http.client
 import json
 import time
 import pytest
-
+import configparser
 
 __author__ = "Vaibhav Singhal"
 __copyright__ = "Copyright (c) 2019 Dianomic Systems"
@@ -23,10 +23,18 @@ __version__ = "${VERSION}"
 TEMPLATE_NAME = "template.json"
 SENSOR_VALUE = 10
 
+config = configparser.RawConfigParser()
+config.read('config/example.cfg')
+BASE_URI = config.get('basic', 'foglamp_url')
+WAIT_TIME = int(config.get('basic', 'wait_time'))
+SOUTH_BRANCH = config.get('south', 'south_branch')
+ASSET_NAME = config.get('south', 'asset_name')
+SERVICE_NAME = config.get('south', 'service_name')
+
 
 @pytest.fixture
-def start_south_coap(reset_and_start_foglamp, add_south, remove_data_file, remove_directories, south_branch,
-                     foglamp_url, south_plugin="coap", asset_name="smoke"):
+def start_south_coap(reset_and_start_foglamp, add_south, remove_data_file, remove_directories,
+                     south_plugin="coap"):
     """ This fixture clone a south repo and starts both south and north instance
         reset_and_start_foglamp: Fixture that resets and starts foglamp, no explicit invocation, called at start
         add_south: Fixture that adds a south service with given configuration
@@ -40,9 +48,9 @@ def start_south_coap(reset_and_start_foglamp, add_south, remove_data_file, remov
         f.write(
             '[{"name": "%s", "sensor_values": '
             '[{"name": "sensor", "type": "number", "min": %d, "max": %d, "precision": 0}]}]' % (
-                asset_name, SENSOR_VALUE, SENSOR_VALUE))
+                ASSET_NAME, SENSOR_VALUE, SENSOR_VALUE))
 
-    add_south(south_plugin, south_branch, foglamp_url, service_name="coap")
+    add_south(south_plugin, SOUTH_BRANCH, BASE_URI, service_name=SERVICE_NAME)
 
     yield start_south_coap
 
@@ -51,7 +59,7 @@ def start_south_coap(reset_and_start_foglamp, add_south, remove_data_file, remov
     remove_directories("/tmp/foglamp-south-{}".format(south_plugin))
 
 
-def test_smoke(start_south_coap, foglamp_url, wait_time, asset_name="smoke"):
+def test_smoke(start_south_coap):
     """ Test that data is inserted in FogLAMP
         start_south_coap: Fixture that starts FogLAMP with south coap plugin
         Assertions:
@@ -59,11 +67,11 @@ def test_smoke(start_south_coap, foglamp_url, wait_time, asset_name="smoke"):
             on endpoint GET /foglamp/asset/<asset_name>
     """
 
-    conn = http.client.HTTPConnection(foglamp_url)
-    time.sleep(wait_time)
+    conn = http.client.HTTPConnection(BASE_URI)
+    time.sleep(WAIT_TIME)
     subprocess.run(["cd $FOGLAMP_ROOT/extras/python; python3 -m fogbench -t ../../data/{}; cd -".format(TEMPLATE_NAME)],
                    shell=True, check=True)
-    time.sleep(wait_time)
+    time.sleep(WAIT_TIME)
     conn.request("GET", '/foglamp/asset')
     r = conn.getresponse()
 
@@ -71,10 +79,10 @@ def test_smoke(start_south_coap, foglamp_url, wait_time, asset_name="smoke"):
     r = r.read().decode()
     retval = json.loads(r)
     assert len(retval) == 1
-    assert asset_name == retval[0]["assetCode"]
+    assert ASSET_NAME == retval[0]["assetCode"]
     assert 1 == retval[0]["count"]
 
-    conn.request("GET", '/foglamp/asset/{}'.format(asset_name))
+    conn.request("GET", '/foglamp/asset/{}'.format(ASSET_NAME))
     r = conn.getresponse()
     assert 200 == r.status
     r = r.read().decode()
